b93e26fec963ceae4e0e658efa4f0145c869be3d
vasisth13 <77538398+vasisth13@users.noreply.github.com>
2021-05-09 16:55:47 +0530

    Add files via upload

---
+++ scriptiplresults.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDKsGf8V-4NTAwLjRTHAI7RF9FAiGyyZmU",
+    authDomain: "sport-website-35458.firebaseapp.com",
+    databaseURL: "https://sport-website-35458-default-rtdb.firebaseio.com",
+    projectId: "sport-website-35458",
+    storageBucket: "sport-website-35458.appspot.com",
+    messagingSenderId: "773986518969",
+    appId: "1:773986518969:web:88238a085024c42554d868"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
